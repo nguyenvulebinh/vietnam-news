724141a1bb901c2c64ce0136cf0c2a56e31f3ca0
Nguyen Minh Phuong <phuongnm.bkhn@gmail.com>
2016-11-17 23:38:39 +0700

    Cap nhat ban update vao solr

--- module-crawler/src/solr_helper.py
+++ module-crawler/src/solr_helper.py
@@ -1,7 +1,12 @@
 
-import  os
+import os
 import requests
 import json
+
+from web import *
+from libs_support import *
+from rss_parser import *
+from database import *
 
 class Solr_helper:
 
@@ -45,37 +50,89 @@ class Solr_helper:
         try:
             r = requests.post(url=url,data=data_json,headers=headers)
             r.close()
-            print (r.text)
             return r.text  # .encode('utf-8', 'inorge')
         except Exception, e:
             print('Exception' + str(e))
             return None
 
+    def reload(self):
+        # post paterm: curl "http://localhost:8983/solr/admin/cores?action=RELOAD&core=mycore"
+        # use Data with Index Handlers (DIH) Http post
+        url = "http://{0}:{1}/solr/admin/cores?action=RELOAD&core={2}" .format(self.server_domain, self.server_port,self.server_db_name)
+        try:
+            r = requests.post(url=url)
+            r.close()
+            return r.text  # .encode('utf-8', 'inorge')
+        except Exception, e:
+            print('Exception' + str(e))
+            return None
+
+def crawl_data():
+    max_count_web = 500
+    rss_page_links = [
+        # "http://vietbao.vn/vn/rss",
+        # "http://vnexpress.net/rss",
+        # "http://dantri.com.vn/rss",
+        # "http://vtv.vn/rss",
+        "http://techtalk.vn/"
+    ]
+    web_mannual_page_links = [
+        # "vtv.vn"  ,
+        "kenh14.vn"
+    ]
+
+    # Cai dat bo loc crawl web
+    # Web_filter.set_last_time("2016-10-26, 22:20:08+07:00")  # Bai viet moi hon ke tu thoi diem xxx
+    # Web_filter.set_limit_time("2016-10-26, 22:20:08+07:00", "2016-10-26, 23:20:08+07:00")  # Bai viet trong khoang tg
+    Web_filter.set_max_count_web_each_domain(10000)  # moi domain khong vuot qua 1000
+    Web_filter.set_max_count_web_each_sublabel(100)  # moi label trong 1 domain k vuot qua 100
+
+    # Cac trang co rss
+    data = "["
+    for link_rss in rss_page_links:
+        parser = rss_parser(link_rss)
+        webs = parser.get_list_web()
+        for web_x in  webs:
+            data += (web_x.get_json()+",")
+            # web_x.write_to_file('/mnt/01CDF1ECE3AB4280/DH/NAM_5/Ki_1/TimkiemTrinhDien/BTL/vietnam-news/data-train')
+
+    if data.__len__() > 1:
+        data = data[:-1]+"]"
+        solr = Solr_helper(db_name="testBTL")
+        solr.set_solr_home("/mnt/01CDF1ECE3AB4280/DH/NAM_5/Ki_1/TimkiemTrinhDien/BTL/solr-6.2.1")
+
+        print (solr.update(data))
+        print (solr.reload())
+
+
+
 
 if __name__ =="__main__":
     solr =  Solr_helper( db_name = "testBTL")
     solr.set_solr_home("/mnt/01CDF1ECE3AB4280/DH/NAM_5/Ki_1/TimkiemTrinhDien/BTL/solr-6.2.1")
-    # solr.update("/mnt/01CDF1ECE3AB4280/DH/NAM_5/Ki_1/TimkiemTrinhDien/BTL/vietnam-news/data-train/techtalk/Cong\ nghe/31fa871c7d521106e28c45f567a63445c33e1186.json")
+    # # solr.update("/mnt/01CDF1ECE3AB4280/DH/NAM_5/Ki_1/TimkiemTrinhDien/BTL/vietnam-news/data-train/techtalk/Cong\ nghe/31fa871c7d521106e28c45f567a63445c33e1186.json")
+    #
+    # data_test = []
+    # data_test.append({
+    #     "code": "55421c7d521106e28c45f567a63445c33e118744446",
+    #     "title": "test dddd vcc c dsf" ,
+    #     "url": "http://techtalk.vn/van-de-da-ngon-ngu-trong-angularjs.html",
+    #     "labels": "techtalk/Cong nghe",
+    #     "content": "tset content ",
+    #     "image_url": "",
+    #     "date": "2016-11-14, 12:00:02+00:00"
+    # })
+    # data_test.append({
+    #     "code": "12345651717ebecaeb1c179522eff5dcc19c86ce8",
+    #     "title": "test title ",
+    #     "url": "http://techtalk.vn/tim-hieu-ve-middleware-trong-expressjs.html",
+    #     "labels": "techtalk/Cong nghe",
+    #     "content": "test ddddd content ",
+    #     "image_url": "",
+    #     "date": "2016-11-13, 01:00:14+00:00"
+    # })
+    crawl_data()
+    # data_json = (json.dumps(data_test,indent=4, separators=(',', ': '), ensure_ascii=False))
+    # solr.update(data_json)
+    # print (solr.reload())
 
-    data_test = []
-    data_test.append({
-        "code": "1233871c7d521106e28c45f567a63445c33e118744446",
-        "title": "test dddd vcc c dsf" ,
-        "url": "http://techtalk.vn/van-de-da-ngon-ngu-trong-angularjs.html",
-        "labels": "techtalk/Cong nghe",
-        "content": "tset content ",
-        "image_url": "",
-        "date": "2016-11-14, 12:00:02+00:00"
-    })
-    data_test.append({
-        "code": "12345651717ebecaeb1c179522eff5dcc19c86ce8",
-        "title": "test title ",
-        "url": "http://techtalk.vn/tim-hieu-ve-middleware-trong-expressjs.html",
-        "labels": "techtalk/Cong nghe",
-        "content": "test ddddd content ",
-        "image_url": "",
-        "date": "2016-11-13, 01:00:14+00:00"
-    })
-
-    data_json = (json.dumps(data_test,indent=4, separators=(',', ': '), ensure_ascii=False))
-    solr.update(data_json)
